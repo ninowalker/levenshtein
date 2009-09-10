8ed67d726368499a6d07004b3876d2f66fb6eb84
Nino Walker <kolohe@lscape.local>
2009-09-09 22:56:27 -0700

    Author details.

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from distutils.core import setup, Extension
 module1 = Extension('levenshtein', sources = ['python.c','levenshtein.c'])
 
 setup (name = 'levenshtein',
-       version = '1.0',
-       description = 'The levenshtein module implements fast Damerau-Levenshtein edit distance computation in O(n) memory and O(n^2) time, using a C wrapper',
+       version = '1.0.0',
+       author = 'Schuyler Erle',
+       author_email = 'schuyler@entropyfree.com',
+       url='http://entropyfree.com/',
+       
+       description = 'Fast edit distance computation',
+       long_description='The levenshtein module implements fast Damerau-Levenshtein edit distance computation in O(n) memory and O(n^2) time, using a C wrapper',
        ext_modules = [module1])
